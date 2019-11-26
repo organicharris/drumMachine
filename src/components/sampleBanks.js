30583ac02f5f98a174b04c599c72478fcef309fb
Craig Harrison <craigharrison24@gmail.com>
2019-11-26 16:43:57 +0000

    Changed sample locations (to improve ergonomics)

--- src/components/sampleBanks.js
+++ src/components/sampleBanks.js
@@ -5,11 +5,11 @@ const bankA = [
     {sampleID: 'Hit 2', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'}, 
     {sampleID: 'Hit 3', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'}, 
     {sampleID: 'Hit 4', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'}, 
-    {sampleID: 'Clap', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'}, 
     {sampleID: 'Open HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'}, 
-    {sampleID: "Kick & Hat", url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'}, 
-    {sampleID: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'}, 
     {sampleID: 'Closed HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}, 
+    {sampleID: 'Clap', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},  
+    {sampleID: "Kick & Hat", url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'}, 
+    {sampleID: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
     {sampleID: "---"} // last entry acts as placeholder
 ];
 
@@ -22,9 +22,9 @@ const bankB = [
     {sampleID: 'Shaker', url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'}, 
     {sampleID: 'Open HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'}, 
     {sampleID: 'Closed HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'}, 
-    {sampleID: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'}, 
-    {sampleID: 'Rim Shot', url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'}, 
     {sampleID: 'Snare', url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'}, 
+    {sampleID: 'Rim Shot', url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'}, 
+    {sampleID: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'}, 
     {sampleID: "---"} // last entry acts as placeholder
 ];
 
