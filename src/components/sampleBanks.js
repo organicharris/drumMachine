955244ecec33f505a83ab735a91fad0168f7cf1d
Craig Harrison <craigharrison24@gmail.com>
2019-11-27 19:15:48 +0000

    Local audio

--- src/components/sampleBanks.js
+++ src/components/sampleBanks.js
@@ -1,30 +1,30 @@
 const bankA = [
     // First array entry shows name of bank
     "bank-A",
-    {sampleID: 'Hit 1', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'}, 
-    {sampleID: 'Hit 2', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'}, 
-    {sampleID: 'Hit 3', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'}, 
-    {sampleID: 'Hit 4', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'}, 
-    {sampleID: 'Open HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'}, 
-    {sampleID: 'Closed HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}, 
-    {sampleID: 'Clap', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},  
-    {sampleID: "Kick & Hat", url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'}, 
-    {sampleID: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
+    {sampleID: 'Hit 1', url: './samples/Heater-1.mp3'}, 
+    {sampleID: 'Hit 2', url: './samples/Heater-2.mp3'}, 
+    {sampleID: 'Hit 3', url: './samples/Heater-3.mp3'}, 
+    {sampleID: 'Hit 4', url: './samples/Heater-4_1.mp3'}, 
+    {sampleID: 'Open HH', url: './samples/Dsc_Oh.mp3'}, 
+    {sampleID: 'Closed HH', url: './samples/Cev_H2.mp3'}, 
+    {sampleID: 'Clap', url: './samples/Heater-6.mp3'},  
+    {sampleID: "Kick & Hat", url: './samples/Kick_n_Hat.mp3'}, 
+    {sampleID: 'Kick', url: './samples/RP4_KICK_1.mp3'},
     {sampleID: "---"} // last entry acts as placeholder
 ];
 
 const bankB = [
     // First array entry shows name of bank
     "bank-B",
-    {sampleID: 'Chord 1', url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'}, 
-    {sampleID: 'Chord 2', url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'}, 
-    {sampleID: 'Chord 3', url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'}, 
-    {sampleID: 'Shaker', url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'}, 
-    {sampleID: 'Open HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'}, 
-    {sampleID: 'Closed HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'}, 
-    {sampleID: 'Snare', url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'}, 
-    {sampleID: 'Rim Shot', url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'}, 
-    {sampleID: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'}, 
+    {sampleID: 'Chord 1', url: './samples/Chord_1.mp3'}, 
+    {sampleID: 'Chord 2', url: './samples/Chord_2.mp3'}, 
+    {sampleID: 'Chord 3', url: './samples/Chord_3.mp3'}, 
+    {sampleID: 'Shaker', url: './samples/Give_us_a_light.mp3'}, 
+    {sampleID: 'Open HH', url: './samples/Dry_Ohh.mp3'}, 
+    {sampleID: 'Closed HH', url: './samples/Bld_H1.mp3'}, 
+    {sampleID: 'Snare', url: './samples/Brk_Snr.mp3'}, 
+    {sampleID: 'Rim Shot', url: './samples/side_stick_1.mp3'}, 
+    {sampleID: 'Kick', url: './samples/punchy_kick_1.mp3'}, 
     {sampleID: "---"} // last entry acts as placeholder
 ];
 
