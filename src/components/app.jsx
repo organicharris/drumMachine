dc2f6a3b405a86797fcf188ed8c50da1775eb488
Craig Harrison <craigharrison24@gmail.com>
2020-01-06 18:02:58 +0000

    Added rotate notice

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,11 +10,19 @@ import '../styles/main.css';
 import '../styles/animations.css';
 import '../styles/mediaCalls.css';
 
+// import Font Awesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+
 class App extends React.Component {
 
     render() {
         return (
             <div id="main">
+                <div id="rotateNotice">
+                    <p>Please rotate the screen of your device to landscape.</p>
+                    <p><FontAwesomeIcon icon={faMobileAlt} id="rotatePhone"/></p>
+                </div>
                 <div id="drum-machine">
                 <div id="title">Beat Composer</div>
                 <div id="website"><a href="https://craigharrison.dev/" title="Craig Harrison" target="_blank" rel="noopener noreferrer">craigharrison.dev</a></div>
